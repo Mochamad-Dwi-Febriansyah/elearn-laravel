4402fc7db425048ec9447b8dce4f50a56bf0defb
Mochamad-Dwi-Febriansyah <iniakunnyaorangjoos@gmail.com>
2023-09-20 20:28:30 +0700

    commit 20-09

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,7 +6,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use App\Mail\ForgotPasswordMail;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
 
 class AuthController extends Controller
 {
@@ -55,9 +57,35 @@ class AuthController extends Controller
     public function PostForgotPassword(Request $request){ 
         $user = User::getEmailSingle($request->email);
         if(!empty($user)){
+            $user->remember_token = Str::random(30);
+            $user->save();
             Mail::to($user->email)->send(new ForgotPasswordMail($user));
+
+            return redirect()->back()->with('success', 'Please Check your email and reset your password.');
         }else{
             return redirect()->back()->with('error', 'Email not found in the system.');
+        }
+    }
+
+    public function reset($remember_token){
+        $user = User::getTokenSingle($remember_token);
+        if(!empty($user)){
+            $data['user'] = $user;
+            return view('auth.reset', $data);
+        }else{
+            abort(404);
+        }
+    }
+    public function PostReset($token, Request $request){
+        if($request->password == $request->cpassword){
+            $user = User::getTokenSingle($token);
+            $user->password = Hash::make($request->password);
+            $user->remember_token = Str::random(30);
+            $user->save();
+
+            return redirect('/')->with('success', "Password successfully reset");
+        }else{
+            return redirect()->back()->with('error', "Password and Confirm password does not match");
         }
     }
 
--- app/Models/User.php
+++ app/Models/User.php
@@ -42,7 +42,21 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+    static public function getSingle($id){
+        return self::find($id);
+    }
+    static public function getAdmin(){
+        return self::select('users.*')
+                        ->where('user_type', '=',1)
+                        ->where('id_delete', '=',0)
+                        ->orderBy('id', 'desc')
+                        ->get();
+    }
+
     static public function getEmailSingle($email){
         return User::where('email', '=', $email)->first();
+    }
+    static public function getTokenSingle($remember_token){
+        return User::where('remember_token', '=', $remember_token)->first();
     }
 }
